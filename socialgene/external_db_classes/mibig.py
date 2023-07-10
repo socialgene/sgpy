a80b7c62000e74dec745a0f54cd8ecca49fa9dcc
chasemc <18691127+chasemc@users.noreply.github.com>
2023-07-10 10:15:08 -0500

    wip

--- socialgene/external_db_classes/mibig.py
+++ socialgene/external_db_classes/mibig.py
@@ -1,11 +1,26 @@
 from socialgene.external_db_classes.base_class import ExternalBaseClass
+import re
 
 
 class Mibig(ExternalBaseClass):
     __slots__ = ["uid"]
 
     def __init__(self, uid) -> None:
-        self.uid = uid
+        self.uid = self.attempt_to_fix_uid(uid)
+
+    @staticmethod
+    def attempt_to_fix_uid(uid):
+        # npatlas has some mibig ids prefixed, some not, try to add prefix here
+        # https://github.com/NPAtlas/npatlas_website/issues/13
+        if uid.startswith("BGC"):
+            temp = uid
+        else:
+            temp = "BGC" + uid.zfill(7)
+        # Validate uid
+        if re.match("BGC[0-9]{7}", temp):
+            return temp
+        else:
+            raise ValueError(f"Unexpected mibig ID {uid}")
 
     def add_node_to_neo4j(self):
         # empty because this should connect to mibig assembly nodes
