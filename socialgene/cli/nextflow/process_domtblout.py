63c15e7367c6b31d47802412d03ae47c6f6a0b8b
chasemc <18691127+chasemc@users.noreply.github.com>
2023-11-17 06:48:25 -0600

    fix: serialize hmmparse

--- socialgene/cli/nextflow/process_domtblout.py
+++ socialgene/cli/nextflow/process_domtblout.py
@@ -36,13 +36,9 @@ parser.add_argument(
     default=None,
     action=argparse.BooleanOptionalAction,
 )
-parser.add_argument(
-    "--cpus", help="Number of cpus", required=False, default=1, type=int
-)
 
 
-def process_domtblout_file(domtblout_file, ievaluefilter):
-    domain_counter = 0
+def process_domtblout_file(domtblout_file, ievaluefilter, domain_counter):
     socialgene_object = SocialGene()
     _to_return = []
     for i in socialgene_object._parse_domtblout(
@@ -58,9 +54,7 @@ def process_domtblout_file(domtblout_file, ievaluefilter):
             _temp = [i["external_id"]]
             _temp.extend(list(domain_obj.tsv_attributes().values()))
             domain_counter += 1
-            _to_return.append(_temp)
-
-    return (_to_return, domain_counter)
+            yield _temp
 
 
 def main():
@@ -83,21 +77,18 @@ def main():
     else:
         domain_counter = 0
         log.info(f"Socialgene variables: \n{env_vars}")
-        # print(Panel(f"Socialgene variables: {env_vars}"))
         args = parser.parse_args()
-        log.info(f"sg_process_domtblout will append data to {args.outpath}")
-        p = Pool(args.cpus)
+        log.info(f"sg_process_domtblout will write data to {args.outpath}")
         with open(args.outpath, "w") as f:
             tsv_writer = csv.writer(f, delimiter="\t")
-            for result in p.imap(
-                functools.partial(
-                    process_domtblout_file, ievaluefilter=args.ievaluefilter
-                ),
-                hmm_files,
-            ):
-                for i in result[0]:
+            for domtblout_file in hmm_files:
+                for i in process_domtblout_file(
+                    domtblout_file,
+                    ievaluefilter=args.ievaluefilter,
+                    domain_counter=domain_counter,
+                ):
                     tsv_writer.writerow(i)
-                domain_counter += result[1]
+                    domain_counter += 1
         log.info(f"Wrote {str(domain_counter)} domains to {args.outpath}")
 
 
