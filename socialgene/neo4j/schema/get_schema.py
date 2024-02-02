b7f737d154f651f21444891c3bebe7f1718ea628
chasemc <18691127+chasemc@users.noreply.github.com>
2024-02-02 06:37:40 -0600

    wip

--- socialgene/neo4j/schema/get_schema.py
+++ socialgene/neo4j/schema/get_schema.py
@@ -7,7 +7,12 @@ from socialgene.utils.lists_to_markdown import markdown_table_from_list
 from rich.console import Console, ConsoleOptions, RenderResult
 from rich.table import Table
 import builtins
+from rich.console import Console
 
+from socialgene.config import env_vars
+from socialgene.utils.logging import log
+
+console = Console()
 builtins.print = print
 
 
@@ -25,9 +30,7 @@ class GraphSchema:
     def __init__(self):
         pass
 
-    def __rich_console__(
-        self, console: Console, options: ConsoleOptions
-    ) -> RenderResult:  # pragma: no cover
+    def _nodes_table(self):
         node_dict = {i.__name__: i for i in self.ALL_NODES}
         node_dict = dict(sorted(node_dict.items()))
         table = Table(title="Nodes", show_lines=True)
@@ -44,56 +47,92 @@ class GraphSchema:
         # sort by label which is the key
         for i in node_dict.values():
             table.add_row(
-                i._Neo4jElement__neo4j_label,
-                i._Neo4jElement__description,
-                "\n".join(wrap(", ".join(i._Neo4jElement__header))),
+                i()._Neo4jElement__neo4j_label,
+                i()._Neo4jElement__description,
+                "\n".join(wrap(", ".join(i()._Neo4jElement__header))),
             )
         yield table
 
+    def _relationships_table(self):
+        rel_dict = {i.__name__: i for i in self.ALL_RELATIONSHIPS}
+        rel_dict = dict(sorted(rel_dict.items()))
+        table = Table(title="Relationships", show_lines=True)
+        table.add_column("Label", justify="left", style="cyan", no_wrap=True, ratio=1)
+        table.add_column("Relationship", style="magenta", ratio=1)
+        table.add_column("NF results subdirectory", style="magenta", ratio=1)
+        table.add_column("Neo4j header file", style="magenta", ratio=1)
+        for i in rel_dict.values():
+            table.add_row(
+                i()._Neo4jElement__neo4j_label,
+                i()._cypher_string,
+                i()._Neo4jElement__target_subdirectory,
+                i()._Neo4jElement__header_filename,
+            )
+        yield table
 
-def _markdown_table_nodes(nodelist):
-    node_dict = {i.__name__: i for i in nodelist}
-    node_dict = dict(sorted(node_dict.items()))
-    cols = [
-        (
-            "Label",
-            "Description",
-            "NF results subdirectory",
-            "Neo4j header file",
-            "properties",
+    def _markdown_table_nodes(nodelist):
+        node_dict = {i.__name__: i for i in nodelist}
+        node_dict = dict(sorted(node_dict.items()))
+        cols = [
+            (
+                "Label",
+                "Description",
+                "NF results subdirectory",
+                "Neo4j header file",
+                "properties",
+            )
+        ]
+        rows = [
+            (
+                i()._Neo4jElement__neo4j_label,
+                i()._Neo4jElement__description,
+                i()._Neo4jElement__target_subdirectory,
+                i()._Neo4jElement__header_filename,
+                i()._Neo4jElement__properties,
+            )
+            for i in node_dict.values()
+        ]
+        cols.extend(rows)
+        print(
+            markdown_table_from_list(
+                cols,
+                align="left",
+            )
         )
-    ]
-    rows = [
-        (
-            i()._Neo4jElement__neo4j_label,
-            i()._Neo4jElement__description,
-            i()._Neo4jElement__target_subdirectory,
-            i()._Neo4jElement__header_filename,
-            i()._Neo4jElement__properties,
+
+    def _markdown_table_rels(rellist):
+        rel_dict = {i.__name__: i for i in rellist}
+        rel_dict = dict(sorted(rel_dict.items()))
+        cols = [
+            (
+                "Label",
+                "Relationship",
+                "NF results subdirectory",
+                "Neo4j header file",
+            )
+        ]
+        rows = [
+            (
+                i()._Neo4jElement__neo4j_label,
+                i()._cypher_string,
+                i()._Neo4jElement__target_subdirectory,
+                i()._Neo4jElement__header_filename,
+            )
+            for i in rel_dict.values()
+        ]
+        cols.extend(rows)
+        print(
+            markdown_table_from_list(
+                cols,
+                align="left",
+            )
         )
-        for i in node_dict.values()
-    ]
-    cols.extend(rows)
-    print(
-        markdown_table_from_list(
-            cols,
-            align="left",
-        )
-    )
-
-
-def print_info():  # pragma: no cover
-    print(GraphSchema().__rich_console__())
-
-
-def print_markdown():  # pragma: no cover
-    _markdown_table_nodes(1)
 
 
 builtins.print = print
 
 
-def printer():  # pragma: no cover
+def main():  # pragma: no cover
     import argparse
 
     parser = argparse.ArgumentParser(description="Print node info")
@@ -104,12 +143,32 @@ def printer():  # pragma: no cover
         required=False,
         action=argparse.BooleanOptionalAction,
     )
+    parser.add_argument(
+        "--nodes",
+        help="",
+        default=False,
+        required=False,
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--rels",
+        help="",
+        default=False,
+        required=False,
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
     if args.markdown:
-        print_markdown()
+        if args.nodes:
+            GraphSchema._markdown_table_nodes(GraphSchema.ALL_NODES)
+        if args.rels:
+            GraphSchema._markdown_table_rels(GraphSchema.ALL_RELATIONSHIPS)
     else:
-        print_info()
+        if args.nodes:
+            console.print(GraphSchema()._nodes_table().__next__())
+        if args.rels:
+            console.print(GraphSchema()._relationships_table().__next__())
 
 
 if __name__ == "__main__":
-    print_info()
+    main()
