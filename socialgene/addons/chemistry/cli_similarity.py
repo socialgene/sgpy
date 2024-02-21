0791d42365bb140d09dcfe2574e564164d35950f
chasemc <18691127+chasemc@users.noreply.github.com>
2024-02-21 12:06:41 -0600

    a

--- socialgene/addons/chemistry/cli_similarity.py
+++ socialgene/addons/chemistry/cli_similarity.py
@@ -3,7 +3,16 @@ from rdkit import DataStructs
 from socialgene.addons.chemistry.nr import ChemicalCompoundNode, ChemicalSimilarity
 from socialgene.base.chem import ChemicalCompound
 from socialgene.neo4j.neo4j import GraphDriver
-
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeElapsedColumn,
+)
+import multiprocessing
+import pandas as pd
+from itertools import batched
 cmpd_label = ChemicalCompoundNode.neo4j_label
 
 
@@ -11,52 +20,97 @@ with GraphDriver() as db:
     res = db.run(
         f"""
         MATCH (c1:{cmpd_label})
-        RETURN c1 as chem
+        RETURN c1.inchi as chem
         """,
     ).value()
 
 
-z = {
-    tuple([i[x] for x in ChemicalCompoundNode.required_properties]): ChemicalCompound(
-        i["inchi"]
-    )
-    for i in res
-}
+def make_node(x):
+    res = {}
+    for i in x:
+        temp = ChemicalCompound(i)
+        res[i] = (temp, temp.node)
+    return res
 
 
-big_vec = [i.morgan for i in z.values()]
+
+num_processes = multiprocessing.cpu_count() - 2
+group_size = len(res) // num_processes
+
+b = batched(res, group_size)
+combined_dict = {}
+with multiprocessing.Pool(processes=num_processes) as pool:
+    for res in pool.imap_unordered(make_node, b):
+        combined_dict.update(res)
+
+
+
+
+id_list = list(combined_dict.keys())
+
+big_vec = [i[0].morgan for i in combined_dict.values()]
 cutoff = 0.5
 
-# loop through big_vec and compare each vector to the rest of the vectors
-# if the similarity is greater than the cutoff, add the node to the list
-
-big_dict = {i: [] for i in z.keys()}
-
-id_list = list(z.keys())
-
-chemsim_set = set()
-for chemkey in id_list:
-    for i, x in enumerate(
-        DataStructs.BulkTanimotoSimilarity(z[chemkey].morgan, big_vec)
-    ):
-        if x >= cutoff:
-            if chemkey != id_list[i]:
-                temp = [chemkey, id_list[i]]
-                temp.sort()
-                score = int(x * 100)
-                chemsim_set.add(
-                    ChemicalSimilarity(
-                        start=z[temp[0]].node,
-                        end=z[temp[1]].node,
-                        properties={"similarity": score},
-                    )
-                )
-
-ChemicalSimilarity.add_multiple_to_neo4j(list(chemsim_set), create=True)
 
 
-bro = [
-    (k for k in z.keys())
-    for i, x in enumerate(DataStructs.BulkTanimotoSimilarity(big_vec[i], big_vec))
-    if x >= cutoff
-]
+
+def calculate_similarity(tupgroup):
+    chemsim_set = set()
+    tup = tupgroup[2]
+    for iter in range(tupgroup[0], tupgroup[1]):
+        for i, x in enumerate(DataStructs.BulkTanimotoSimilarity(tup[iter], tup)):
+            if x >= 0.5:
+                if tup[iter] != i:
+                    score = int(x * 100)
+                    temp = (iter, i, score)
+                    chemsim_set.add(temp)
+    return chemsim_set
+
+
+# Split x into groups
+x=[(i,big_vec) for i in range(len(id_list))]
+
+group_size = len(id_list) // num_processes
+groups = [(i,i+group_size, big_vec) for i in range(0, len(id_list), group_size)]
+groups[-1] = (groups[-1][0], len(id_list) - 1, big_vec)
+chemsim = set()
+
+with multiprocessing.Pool(processes=num_processes) as pool:
+    for i in pool.imap_unordered(calculate_similarity, groups):
+        chemsim.update(i)
+
+
+chemsim2 = set()
+
+for i in chemsim:
+    # sorted prevents identical forward + reverse relationships
+    chemsim2.add(tuple(sorted(i[0:2]) + [i[2]]))
+
+chemsim=chemsim2
+del chemsim2
+
+# get nodes faster using pd indexing
+df0 = pd.DataFrame(((k, v[0], v[1]) for k, v in combined_dict.items()), columns = ["id", "compound", "node"])
+df = pd.DataFrame(chemsim, columns=["start", "end", "similarity"])
+df = df.set_index('start')
+df = df.join(df0['node'])
+df.columns = ["end", "similarity", "start_node"]
+df = df.reset_index(level=0)
+df = df.set_index('end')
+df = df.join(df0['node'])
+df.columns = ["start", "similarity", "start_node", "end_node"]
+df = df.reset_index(level=0)
+df['relationship'] = df.apply(lambda x: ChemicalSimilarity(start=x.start_node, end=x.end_node, properties={"similarity": x.similarity}), axis=1)
+
+
+zz={i:z[id_list[i]].node for i,x in enumerate(id_list)}
+
+df['relationship'] = df.apply(lambda x: ChemicalSimilarity(start=x.start_node.node, end=x.end_node.node, properties={"similarity": x.similarity}), axis=1)
+
+
+a=[ChemicalSimilarity(start=z[id_list[i[0]]].node,end=z[id_list[i[1]]].node,properties={"similarity": i[2]},) for i in chemsim]
+ChemicalSimilarity(start=z[id_list[i[0]]].node,end=z[id_list[i[1]]].node,properties={"similarity": i[2]},)
+
+
+
+
